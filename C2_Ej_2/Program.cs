34143898bec7e3097b76e1a09c05aaf6399b71ab
Rubirpg <rubi.rpg@gmail.com>
2022-07-14 12:22:23 +0200

    Finalizado con comentarios

--- C2_Ej_2/Program.cs
+++ C2_Ej_2/Program.cs
@@ -10,25 +10,28 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduzca el primer número: ");
-            int num1 = Convert.ToInt32(Console.ReadLine()); //leemos el primer numero
-            Console.WriteLine("Introduzca el segundo número: ");
-            int num2 = Convert.ToInt32(Console.ReadLine()); //leemos el segundo numero
+            Console.WriteLine("Introduzca un número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine()); //leemos y asignamos a num1 el número
+            Console.WriteLine("Introduzca un número con decimales largo: ");
+            double num2 = Convert.ToDouble(Console.ReadLine()); //leemos y asignamos un número con decimales largo a num2
+            Console.WriteLine("Introduzca un caracter: ");
+            char caracter = Convert.ToChar(Console.ReadLine()); //leemos un caracter
 
-            Console.WriteLine(); //Mostramos los numeros seleccionados y su suma
-            Console.WriteLine("La suma de los numeros {0} y {1} es: {2}", num1, num2, num1 + num2);
 
-            Console.WriteLine();//Mostramos los numeros seleccionados y su resta
-            Console.WriteLine("La resta de los numeros {0} y {1} es: {2}", num1, num2, num1 - num2);
+            //mostramos por pantalla los valores contenidos en las variables
+            Console.WriteLine(); 
+            Console.WriteLine("El valor del número es: " + num1);
+            Console.WriteLine("El valor del número con decimales largo es: " + num2);
+            Console.WriteLine("El valor del caracter es: " + caracter);
 
-            Console.WriteLine();//Mostramos los numeros seleccionados y su multiplicacion
-            Console.WriteLine("La multiplicación de los numeros {0} y {1} es: {2}", num1, num2, num1 * num2);
+            //mostramos por pantalla el resultado de sumar num1 y num2
+            Console.WriteLine("Las suma del número {0} y el numero {1 } es: {2}.", num1, num2, num1 + num2);
 
-            Console.WriteLine();//Mostramos los numeros seleccionados y su division
-            Console.WriteLine("La division de los numeros {0} y {1} es: {2}", num1, num2, num1 / num2);
+            //mostramos por pantalla el resultado de restar a num2 el valor de num1
+            Console.WriteLine("Las resta del número {0} y el número {1} es: {2}.", num2, num1, num2 - num1);
 
-            Console.WriteLine();//Mostramos los numeros seleccionados y su modulo
-            Console.WriteLine("El modulo (resto resultante de dividir ambos números) de los numeros {0} y {1} es: {2}", num1, num2, num1 % num2);
+            //mostramos por pantalla el valor de convertir el caractar a un número
+            Console.WriteLine("El valor numerico del caracter ({0}) es: {1}.", caracter, Convert.ToInt32(caracter));
         }
     }
 }
